806fda9c757bf79b4d4b3a2af51be5adf02b90da
previewapp <previewapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-02 13:26:57 +0000

    Modification du nom du js, modification des js, ajout des header
    git-svn-id: https://plugins.svn.wordpress.org/previewapp/trunk@1036685 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- preview.php
+++ preview.php
@@ -4,7 +4,7 @@ Plugin Name: PreviewApp
 Plugin URI: http://www.preview-app.net
 Description: Plugin d'installation de preview sur votre site wordpress
 Version: 0.1
-Author: Preview APP
+Author: Preview
 Author URI: http://www.preview-app.net
 License: GPL2
 */
@@ -20,7 +20,11 @@ define( 'PREVIEWAPP__MINIMUM_WP_VERSION', '3.1' );
 define( 'PREVIEWAPP__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PREVIEWAPP__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PREVIEWAPP_DELETE_LIMIT', 100000 );
-define( 'PREVIEWAPP__JS', "previewapp.js" );
+define( 'PREVIEWAPP__JS', "injection.js" );
+
+// modification des headers pour les mettre en SameOrigin quand preview est activé.
+@header("X-FRAME-OPTIONS:  SAMEORIGIN");//ALLOW-FROM https://projets.preview-app.net"); 
+@header("Access-Control-Allow-Origin: *");
 
 if(is_file(PREVIEWAPP__PLUGIN_DIR.PREVIEWAPP__JS))
 {
