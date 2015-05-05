3339a5766511d9638b28e135621c62c1cc6ca3e8
previewapp <previewapp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 14:41:59 +0000

    1.1.3 -> 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/previewapp/trunk@1153869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- preview.php
+++ preview.php
@@ -3,7 +3,7 @@
 Plugin Name: PreviewApp
 Plugin URI: http://www.preview-app.net
 Description: Plugin d'installation de preview sur votre site wordpress
-Version: 0.3
+Version: 0.4
 Author: Preview
 Author URI: http://www.preview-app.net
 License: GPL2
@@ -17,24 +17,11 @@ if ( !function_exists( 'add_action' ) ) {
 
 define( 'PREVIEWAPP_VERSION', '4.0.3' );
 define( 'PREVIEWAPP__MINIMUM_WP_VERSION', '3.1' );
-define( 'PREVIEWAPP__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'PREVIEWAPP__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'PREVIEWAPP_DELETE_LIMIT', 100000 );
-define( 'PREVIEWAPP__JS', "injection.js" );
 
 // modification des headers pour les mettre en SameOrigin quand preview est activ
-@header("X-FRAME-OPTIONS: ALLOW-FROM https://projets.preview-app.net");
-@header("Access-Control-Allow-Origin: *");
+@header('X-FRAME-OPTIONS: ALLOW-FROM https://projets.preview-app.net');
+@header('Access-Control-Allow-Origin: *');
 
-if(is_file(PREVIEWAPP__PLUGIN_DIR.PREVIEWAPP__JS))
-{
-// add preview-app javascript
-wp_enqueue_script('PreViewApp');
-wp_register_script('PreViewApp', PREVIEWAPP__PLUGIN_URL.PREVIEWAPP__JS);
-wp_enqueue_script('PreViewApp');
-}
-else
-{
-	echo 'Error: The Javascript File is empty. See http://www.preview-app.net';
-	exit;
-}
+add_action('wp_enqueue_scripts', function () {
+	wp_register_script('PreViewApp', 'http://projets.preview-app.net/injection.js');
+});
